06163233de7ae7ed114c534473f911561a9b3601
Jon Robson <jdlrobson@gmail.com>
2010-12-22 16:21:26 +0000

    refactored TiddlySpaceInstaller added user params
    loginTiddler
    and registeredOnly allow more control of the install form.
    
    Refactored form creation code into separate method.

--- src/ilga/ilga-plugins/TiddlySpaceInstaller.js
+++ src/ilga/ilga-plugins/TiddlySpaceInstaller.js
@@ -1,6 +1,6 @@
 /***
 |''Name''|TiddlySpaceInstaller|
-|''Version''|0.3.7|
+|''Version''|0.3.8|
 !Usage
 {{{<<showInstall bar Foo>>}}}
 Opens the tiddler Foo to visitors of the bar space. 
@@ -37,42 +37,60 @@ var macro = config.macros.install = {
 		var args = paramString.parseParams("anon")[0];
 		params = args.anon || [];
 		var locale = macro.locale;
-		var setupLabel = args.label ? args.label[0] : locale.setup;
-		var headerTxt = args.header ? args.header[0] : locale.spaceName;
-
+		var mustBeRegistered = args.registeredOnly ? args.registeredOnly[0] != "no" : false;
+		var options = {
+			headerTxt: args.label ? args.label[0] : locale.setup,
+			setupLabel: args.header ? args.header[0] : locale.spaceName,
+			loginTiddler: args.loginTiddler ? args.loginTiddler[0] : false
+		};
 		config.extensions.tiddlyweb.getStatus(function(r) {
 			var host = r.server_host.host;
 			if(params.length == 0) {
 				params = [ tspace.currentSpace.name ]
 			}
-			var form = $("<form />").appendTo(place);
+			var container = $("<div />").appendTo(place)[0];
 			tweb.getUserInfo(function(userInfo) {
-				$("<div />").text(headerTxt).appendTo(form);
-				var user = $("<input />").attr("name", "username").attr("type", "text").appendTo(form);
-				$("<span />").text("." + host).appendTo(form);
-				if(userInfo.anon) {
-					$("<div />").text(locale.password).appendTo(form);
-					var pass1 = $("<input />").attr("name", "pass1").attr("type", "password").appendTo(form);
-					$("<div />").text(locale.passwordAgain).appendTo(form);
-					var pass2 = $("<input />").attr("name", "pass2").attr("type", "password").appendTo(form);
-				}
-				$("<input />").attr("type", "submit").val(setupLabel).appendTo(form);
-				form.submit(function(ev) {
-					var user = $("[name=username]", ev.target).val();
-					var pass = $("[name=pass1]", ev.target).val();
-					var pass2 = $("[name=pass2]", ev.target).val();
-					ev.preventDefault();
-					if(userInfo.anon && pass != pass2) {
-						alert(locale.passwordError);
-					} else if(userInfo.anon && user && pass == pass2){
-						macro.installNewUser(user, pass, params);
-					} else if(!userInfo.anon && user) {
-						macro.setup(user, params)
+				var disabled = userInfo.anon && mustBeRegistered;
+				if(!disabled) {
+					var form = $("<form />").appendTo(container)[0];
+					macro._fillForm(form, host, userInfo, options)
+				} else {
+					var c = $("<span />").appendTo(container)[0];
+					if(options.loginTiddler) {
+						wikify(store.getTiddlerText(options.loginTiddler), c);
 					} else {
-						alert("Please enter a website address");
+						$(c).text("please login!");
 					}
-				});
+				}
 			});
+		});
+	},
+	_fillForm: function(form, host, userInfo, options) {
+		var locale = macro.locale;
+		$("<div />").text(options.headerTxt).appendTo(form);
+		var user = $("<input />").attr("name", "username").attr("type", "text").appendTo(form);
+		$("<span />").text("." + host).appendTo(form);
+		if(userInfo.anon) {
+			$("<div />").text(locale.password).appendTo(form);
+			var pass1 = $("<input />").attr("name", "pass1").attr("type", "password").appendTo(form);
+			$("<div />").text(locale.passwordAgain).appendTo(form);
+			var pass2 = $("<input />").attr("name", "pass2").attr("type", "password").appendTo(form);
+		}
+		$("<input />").attr("type", "submit").val(options.setupLabel).appendTo(form);
+		$(form).submit(function(ev) {
+			var user = $("[name=username]", ev.target).val();
+			var pass = $("[name=pass1]", ev.target).val();
+			var pass2 = $("[name=pass2]", ev.target).val();
+			ev.preventDefault();
+			if(userInfo.anon && pass != pass2) {
+				alert(locale.passwordError);
+			} else if(userInfo.anon && user && pass == pass2){
+				macro.installNewUser(user, pass, params);
+			} else if(!userInfo.anon && user) {
+				macro.setup(user, params)
+			} else {
+				alert("Please enter a website address");
+			}
 		});
 	},
 	installNewUser: function(username, password, includes) {
